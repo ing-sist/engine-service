0f61f7914594c25c333bfa70837aa4ef6af334c3
ignaciogaspar <igaspar@mail.austral.edu.ar>
2025-10-20 22:47:22 -0300

    Initial commit: Spring Boot engine-service

---
+++ src/main/kotlin/ing_sist/engine_service/EngineServiceApplication.kt
@@ -0,0 +1,11 @@
+package ing_sist.engine_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EngineServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<EngineServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ing_sist/engine_service/EngineServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ing_sist.engine_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EngineServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
