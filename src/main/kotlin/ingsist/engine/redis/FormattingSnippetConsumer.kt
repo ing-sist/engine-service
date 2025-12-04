447a1b265a61bf5fbc68f72a76d850577721b4cc
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-12-04 19:48:27 -0300

    linting redis

---
+++ src/main/kotlin/ingsist/engine/redis/FormattingSnippetConsumer.kt
@@ -0,0 +1,40 @@
+package ingsist.engine.redis
+
+import org.austral.ingsis.redis.RedisStreamConsumer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.data.redis.connection.stream.ObjectRecord
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.stream.StreamReceiver
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.util.UUID
+
+@Component
+@Profile("!test")
+class FormattingSnippetConsumer
+    @Autowired
+    constructor(
+        redis: RedisTemplate<String, String>,
+        @Value("\${stream.formatting.key.key}") streamKey: String,
+        @Value("\${groups.format}") groupId: String,
+        private val formattingService: StreamService,
+    ) : RedisStreamConsumer<String>(streamKey, groupId, redis) {
+        override fun options(): StreamReceiver.StreamReceiverOptions<
+            String,
+            ObjectRecord<String, String>,
+        > {
+            return StreamReceiver.StreamReceiverOptions.builder()
+                .pollTimeout(Duration.ofMillis(10000))
+                .targetType(String::class.java)
+                .build()
+        }
+
+        override fun onMessage(record: ObjectRecord<String, String>) {
+            println("arrived")
+            println("Formatting: Id: ${record.id}, Stream: ${record.stream}, Group: $groupId")
+            val uuidId = UUID.fromString(record.value)
+            formattingService.formatAndSaveSnippet(uuidId)
+        }
+    }
